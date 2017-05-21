c7b8e6672e17e6da44b9a34ec56330e88dbdf082
Daniel Phillips <danieledward.phillips@gmail.com>
2017-05-21 13:28:20 -0600

    sets up blank board in player_board

--- lib/player_board.rb
+++ lib/player_board.rb
@@ -1,5 +1,28 @@
-require_relative 'battleship'
+# require_relative 'battleship'
 
 class PlayerBoard
+  attr_accessor :board
+
+  def initialize
+    @board = nil
+  end
+
+  def setup_board(size)
+      letters = Array ('A'..'Z')
+      hash = {}
+
+      size.times do |i|
+        num = i+1 ; alpha = letters[i]
+        key = alpha+num.to_s
+        hash.store(key, nil)
+        size.times do |i|
+          num = i+1
+          key = alpha+num.to_s
+          hash.store(key, nil)
+        end
+      end
+      @board = hash
+    end
+
 
 end
--- test/player_board_test.rb
+++ test/player_board_test.rb
@@ -1,11 +1,15 @@
-require 'Minitest'
-require 'Minitest/pride'
+gem 'minitest'
+require 'minitest/autorun'
+require 'minitest/pride'
 require_relative '../lib/player_board'
 
 class PlayerBoardTest < Minitest::Test
 
-  def test_
-
+  def test_player_can_set_board_from_input
+    instance = PlayerBoard.new
+    expected = {"A1"=>nil,}
+    result = instance.setup_board(1)
+    assert_equal expected, result
   end
 
 end
