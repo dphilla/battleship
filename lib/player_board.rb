aabebd19a70f6f4ecbdf622edc099f134f6733b2
Daniel Phillips <danieledward.phillips@gmail.com>
2017-05-23 18:14:02 -0600

    adds an on_the_board? condition to set_ship1

--- lib/player_board.rb
+++ lib/player_board.rb
@@ -24,37 +24,41 @@ class PlayerBoard
       @board = hash
     end
 
-  def set_ship1 # sub methods should be made private?
+  def set_ship1 #need to check all edge cases (in restraints)
     @board
     ship_coord = gets.chomp
+    coordinate_is_on_board(ship_coord)
     board[ship_coord] = "S"
-    valid_coord = coordinate_is_on_board(ship_coord)
     valid_coord = next_coord_is_repeat?(ship_coord)
     valid_coord = second_coord_valid?(ship_coord, valid_coord)
     board[valid_coord] = "S"
     @board
   end
 
-#submethods for ^^
-    def coordinate_is_on_board(coord) #this also needs a loop to return to user input if bad input is given
-      if board.keys.include?(coord) == false
-        puts "That one is not on the board!"
-      else
-        coord
+#submethods for ^^ make private
+    def coordinate_is_on_board(coord)
+      until valid == true
+        if board.keys.include?(coord) == false
+          puts "That one is not on the board!"
+          coord = gets.chomp
+        else
+          valid = true
+          coord
+        end
       end
     end
 
   def next_coord_is_repeat?(ship_coord)
     ship_coord
     valid_coord = false
-    until valid_coord == true
-    next_coord = gets.chomp
-    if ship_coord[0] == next_coord[0] && ship_coord[1] == next_coord[1]
-      puts "You already plugged in that coordinate!"
-    else
-      valid_coord = true
-    end
-    end
+      until valid_coord == true
+      next_coord = gets.chomp
+        if ship_coord[0] == next_coord[0] && ship_coord[1] == next_coord[1]
+          puts "You already plugged in that coordinate!"
+        else
+          valid_coord = true
+        end
+      end
     next_coord
   end
 
@@ -62,19 +66,19 @@ class PlayerBoard
     ship_coord
     next_coord = valid_coord
     letter_check = Array("A".."Z")
-    coord_is_good = false; until coord_is_good == true
-    if letter_check[letter_check.index(ship_coord[0])+1] == next_coord[0] || letter_check[letter_check.index(ship_coord[0])-1] == next_coord[0]
-      coord_is_good = true
-      next_coord
-    elsif ship_coord[1].to_i + 1 == next_coord[1].to_i || ship_coord[1].to_i - 1 == next_coord[1].to_i
-      coord_is_good = true
-      next_coord
-    else
-      coord_is_good = false
-      print "That is not a valid coordinate"
-      next_coord = gets.chomp
-    end
-    end
+      coord_is_good = false; until coord_is_good == true
+        if letter_check[letter_check.index(ship_coord[0])+1] == next_coord[0] || letter_check[letter_check.index(ship_coord[0])-1] == next_coord[0]
+          coord_is_good = true
+          next_coord
+        elsif ship_coord[1].to_i + 1 == next_coord[1].to_i || ship_coord[1].to_i - 1 == next_coord[1].to_i
+          coord_is_good = true
+          next_coord
+        else
+          coord_is_good = false
+          puts "That is not a valid coordinate"
+          next_coord = gets.chomp
+        end
+      end
     next_coord
   end
 
@@ -85,7 +89,7 @@ class PlayerBoard
 
 end
 
-# instance = PlayerBoard.new
-# instance.setup_board(4)
-#
-# require 'pry'; binding.pry
+instance = PlayerBoard.new
+instance.setup_board(4)
+
+require 'pry'; binding.pry
